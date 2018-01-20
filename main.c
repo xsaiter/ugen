5648d5dbe2c86ad446ab9a1c89dd02abbae390c4
xsaiter <xsaiter@mail.ru>
2018-01-20 09:48:37 +0300

    define

--- main.c
+++ main.c
@@ -5,12 +5,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
- * 
- */
+// black
+#define B 1
+
+// white
+#define W 2
+
+// out
+#define X 3
+
+// size
+#define N 8
+
+// extended size 
+#define E 10
+
+// extended square
+#define ES (E*E)
+
+// start position
+int start_pos[ES] ={
+    X, X, X, X, X, X, X, X, X, X,
+    X, B, B, X, X, X, X, X, X, X,
+    X, B, B, X, X, X, X, X, X, X,
+    X, X, X, X, X, X, X, X, X, X,
+    X, X, X, X, X, X, X, X, X, X,
+    X, X, X, X, X, X, X, X, X, X,
+    X, X, X, X, X, X, X, X, X, X,
+    X, X, X, X, X, X, X, W, W, X,
+    X, X, X, X, X, X, X, W, W, X,
+    X, X, X, X, X, X, X, X, X, X
+};
+
 int main(int argc, char** argv)
 {
-
     return (EXIT_SUCCESS);
 }
 
